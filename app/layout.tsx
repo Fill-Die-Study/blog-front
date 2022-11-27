4df765ac72a46a7f0e1fe9d325171b382b8a8fc2
Daegyeom Ha <combbm@gmail.com>
2022-11-27 22:46:16 +0900

    feat: reinitialize project for next13

---
+++ app/layout.tsx
@@ -0,0 +1,10 @@
+function Layout({ children }: { children: React.ReactNode; }) {
+  return (
+    <html lang="en">
+    <head/>
+    <body>{children}</body>
+    </html>
+  );
+}
+
+export default Layout;
