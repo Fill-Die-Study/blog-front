258488b7bd8e3f1942b5dc40502b3a042d24f41b
Daegyeom Ha <combbm@gmail.com>
2022-11-30 00:02:02 +0900

    feat: add box-border style to global

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,9 +3,8 @@ import React from 'react';
 
 function Layout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="en">
-      <head />
-      <body>{children}</body>
+    <html lang="ko">
+      <body className="box-border">{children}</body>
     </html>
   );
 }
