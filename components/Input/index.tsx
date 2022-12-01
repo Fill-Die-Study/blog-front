e47ced92f43aa37e4dc8cf34c5c4bafae44d132f
Daegyeom Ha <combbm@gmail.com>
2022-12-02 00:19:42 +0900

    feat: add focus event into div(input wrapper)

--- components/Input/index.tsx
+++ components/Input/index.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import React from 'react';
+import React, { useRef } from 'react';
 
 interface InputProps {
   readonly icon?: JSX.Element;
@@ -12,12 +12,17 @@ function Input({ icon: Icon, placeholder = '', width = 'normal' }: InputProps) {
   const widthSize = width === 'normal' ? 'w-48' : 'w-80';
   const inputWidthSize = width === 'normal' ? 'w-40' : 'w-72';
 
+  const inputRef = useRef<HTMLInputElement>(null);
+
   return (
     <div
+      onFocus={() => {
+        if (inputRef.current) inputRef.current.focus();
+      }}
       className={`${widthSize} h-10 px-1 py-1 border-gray-200 border-2 flex items-center cursor-text focus-within:border-black`}
     >
       {Icon ?? Icon}
-      <input className={`ml-1 ${inputWidthSize} focus:outline-0`} placeholder={placeholder} />
+      <input ref={inputRef} className={`ml-1 ${inputWidthSize} focus:outline-0`} placeholder={placeholder} />
     </div>
   );
 }
