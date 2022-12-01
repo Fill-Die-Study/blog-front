513564c9494a61b8c5624a983d255bf1bedfb65a
Daegyeom Ha <combbm@gmail.com>
2022-12-02 01:13:44 +0900

    feat: add title

--- app/head.tsx
+++ app/head.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 function Head() {
   return (
     <>
-      <title />
+      <title>Blog</title>
       <meta name="viewport" content="width=device-width, initial-scale=1" />
       <link rel="icon" href="/favicon.ico" />
     </>
