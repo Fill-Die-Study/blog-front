3b9aa12f4cf676834072e0630c52eb5b1bdc4685
Daegyeom Ha <combbm@gmail.com>
2022-12-02 01:13:38 +0900

    fix: remove useRef() and focus event
    * Revert `e47ced9` commit.

--- components/Input/index.tsx
+++ components/Input/index.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import React, { useRef } from 'react';
+import React from 'react';
 
 interface InputProps {
   readonly icon?: JSX.Element;
@@ -12,17 +12,12 @@ function Input({ icon: Icon, placeholder = '', width = 'normal' }: InputProps) {
   const widthSize = width === 'normal' ? 'w-48' : 'w-80';
   const inputWidthSize = width === 'normal' ? 'w-40' : 'w-72';
 
-  const inputRef = useRef<HTMLInputElement>(null);
-
   return (
     <div
-      onFocus={() => {
-        if (inputRef.current) inputRef.current.focus();
-      }}
       className={`${widthSize} h-10 px-1 py-1 border-gray-200 border-2 flex items-center cursor-text focus-within:border-black`}
     >
       {Icon ?? Icon}
-      <input ref={inputRef} className={`ml-1 ${inputWidthSize} focus:outline-0`} placeholder={placeholder} />
+      <input className={`ml-1 ${inputWidthSize} focus:outline-0`} placeholder={placeholder} />
     </div>
   );
 }
