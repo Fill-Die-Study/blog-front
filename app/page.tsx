d0d19e52762f19064c0b25f6ad71a211be0c9222
Daegyeom Ha <combbm@gmail.com>
2022-12-02 01:14:05 +0900

    feat: add test code for components

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import Button from '@components/Button';
+import Input from '@components/Input';
+import { IoSearch } from 'react-icons/io5';
 
 function Page() {
   return (
     <>
       <Button color="green">테스트</Button>
+      <Input icon={<IoSearch size={18} />} width="long" placeholder="검색어를 입력하세요." />
     </>
   );
 }
