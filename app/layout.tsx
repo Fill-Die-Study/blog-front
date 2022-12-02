d099718393542da43453e381a66f59bd75a950ce
Daegyeom Ha <combbm@gmail.com>
2022-12-03 01:05:33 +0900

    feat: add Link component and space style

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,22 +5,27 @@ import NavigationTitle from '@components/NavigationBar/NavigationTitle';
 import NavigationItemList from '@components/NavigationBar/NavigationItemList';
 import { IoSearch, IoSunny } from 'react-icons/io5';
 import Button from '@components/Button';
+import Link from 'next/dist/client/link';
 
 function Layout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="ko">
       <body className="box-border bg-slate-50">
-        <NavigationBar>
-          <NavigationTitle title="Blog" />
-          <NavigationItemList>
-            <IoSunny size={24} />
-            <IoSearch size={24} />
-            <Button color="black" className="w-20 h-8 text-base" isRound>
-              로그인
-            </Button>
-          </NavigationItemList>
-        </NavigationBar>
-        {children}
+        <div className="space-y-6">
+          <NavigationBar>
+            <Link href="/">
+              <NavigationTitle title="Blog-Front" />
+            </Link>
+            <NavigationItemList>
+              <IoSunny size={24} />
+              <IoSearch size={24} />
+              <Button color="black" className="w-20 h-8 text-base" isRound>
+                로그인
+              </Button>
+            </NavigationItemList>
+          </NavigationBar>
+          <div className="container mx-auto">{children}</div>
+        </div>
       </body>
     </html>
   );
