95ecc66faf41e2a17a79f948d0817590cbbddb36
Daegyeom Ha <combbm@gmail.com>
2022-12-03 00:37:26 +0900

    feat: add black color and className props into Button

--- components/Button/index.tsx
+++ components/Button/index.tsx
@@ -7,7 +7,8 @@ const ButtonColor = {
   green: 'bg-green-500',
   blue: 'bg-blue-500',
   yellow: 'bg-yellow-500',
-  white: 'bg-white'
+  white: 'bg-white',
+  black: 'bg-black'
 };
 
 const ButtonHoverColor = {
@@ -15,16 +16,18 @@ const ButtonHoverColor = {
   green: 'hover:bg-green-400',
   blue: 'hover:bg-blue-400',
   yellow: 'hover:bg-yellow-400',
-  white: 'hover:bg-gray-200'
+  white: 'hover:bg-gray-200',
+  black: 'hover:bg-neutral-700'
 };
 
 interface ButtonProps {
   readonly color: keyof typeof ButtonColor;
   readonly isRound?: boolean;
   readonly isDisable?: boolean;
+  readonly className?: string;
 }
 
-function Button({ color, isDisable, isRound, children }: PropsWithChildren<ButtonProps>) {
+function Button({ color, isDisable, isRound, className = '', children }: PropsWithChildren<ButtonProps>) {
   const disableStyle = isDisable ? 'opacity-50 cursor-not-allowed' : '';
   const roundStyle = isRound ? 'rounded-full' : 'rounded-[4px]';
   const textColor = color === 'white' ? 'text-green-500' : 'text-white';
@@ -32,7 +35,7 @@ function Button({ color, isDisable, isRound, children }: PropsWithChildren<Butto
   return (
     <button
       type="button"
-      className={`w-24 h-10 ${textColor} text-lg font-bold ${ButtonHoverColor[color]} ${roundStyle} ${ButtonColor[color]} ${disableStyle}`}
+      className={`w-24 h-10 ${textColor} text-lg font-bold ${ButtonHoverColor[color]} ${roundStyle} ${ButtonColor[color]} ${disableStyle} ${className}`}
     >
       {children}
     </button>
