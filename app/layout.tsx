600b95d48c474a0a5590adcea933342ccec4e485
Daegyeom Ha <combbm@gmail.com>
2022-12-03 00:51:59 +0900

    feat: add background style and navigation bar component into layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,27 @@
 import '@styles/global.css';
 import React from 'react';
+import NavigationBar from '@components/NavigationBar';
+import NavigationTitle from '@components/NavigationBar/NavigationTitle';
+import NavigationItemList from '@components/NavigationBar/NavigationItemList';
+import { IoSearch, IoSunny } from 'react-icons/io5';
+import Button from '@components/Button';
 
 function Layout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="ko">
-      <body className="box-border">{children}</body>
+      <body className="box-border bg-slate-50">
+        <NavigationBar>
+          <NavigationTitle title="Blog" />
+          <NavigationItemList>
+            <IoSunny size={24} />
+            <IoSearch size={24} />
+            <Button color="black" className="w-20 h-8 text-base" isRound>
+              로그인
+            </Button>
+          </NavigationItemList>
+        </NavigationBar>
+        {children}
+      </body>
     </html>
   );
 }
