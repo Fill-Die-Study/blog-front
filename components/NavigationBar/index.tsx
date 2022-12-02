02df31f398ee715077237d666f19360e9d8290a3
Daegyeom Ha <combbm@gmail.com>
2022-12-03 00:50:27 +0900

    fix: change navbar background color to slate

--- components/NavigationBar/index.tsx
+++ components/NavigationBar/index.tsx
@@ -4,7 +4,7 @@ import React, { PropsWithChildren } from 'react';
 
 function NavigationBar({ children }: PropsWithChildren) {
   return (
-    <div className="w-full h-12 bg-gray-100 flex items-center">
+    <div className="w-full h-12 bg-slate-50 flex items-center">
       <div className="container mx-auto flex items-center justify-between px-10">{children}</div>
     </div>
   );
