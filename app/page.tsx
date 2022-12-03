007e5b57db382e2f40b690007ed67478b80e6b61
Daegyeom Ha <combbm@gmail.com>
2022-12-03 15:05:35 +0900

    feat: implement PreviewCard component

--- app/page.tsx
+++ app/page.tsx
@@ -2,12 +2,34 @@ import React from 'react';
 import Button from '@components/Button';
 import Input from '@components/Input';
 import { IoSearch } from 'react-icons/io5';
+import PreviewCard from '@components/PreviewCard';
 
 function Page() {
   return (
     <>
       <Button color="green">테스트</Button>
       <Input icon={<IoSearch size={18} />} width="long" placeholder="검색어를 입력하세요." />
+      <div className="flex flex-wrap">
+        <PreviewCard
+          title="제목"
+          author="admin"
+          date={new Intl.DateTimeFormat('ko', { dateStyle: 'long' }).format(new Date())}
+          likeCount={10}
+          replyCount={10}
+        >
+          간단한 설명
+        </PreviewCard>
+        <PreviewCard
+          title="제목"
+          thumbnail={<div />}
+          author="admin"
+          date={new Intl.DateTimeFormat('ko', { dateStyle: 'long' }).format(new Date())}
+          likeCount={10}
+          replyCount={10}
+        >
+          썸네일이 있는 글
+        </PreviewCard>
+      </div>
     </>
   );
 }
---
+++ components/PreviewCard/index.tsx
@@ -0,0 +1,55 @@
+'use client';
+
+import React, { PropsWithChildren } from 'react';
+import { IoHeart } from 'react-icons/io5';
+
+interface PreviewCardProps {
+  readonly title: string;
+  readonly thumbnail?: JSX.Element;
+  readonly date: string;
+  readonly author: string;
+  readonly replyCount: number;
+  readonly likeCount: number;
+  readonly profile?: JSX.Element;
+}
+
+function PreviewCard({
+  title,
+  thumbnail,
+  date,
+  author,
+  replyCount,
+  likeCount,
+  profile,
+  children
+}: PropsWithChildren<PreviewCardProps>) {
+  return (
+    <div className="w-80 h-96 bg-white rounded-lg shadow flex flex-col justify-between">
+      {thumbnail ? <div className="w-80 h-40">{thumbnail}</div> : <></>}
+      <div className="px-4 py-4 h-[50%] flex flex-col justify-between">
+        <div>
+          <p className="font-bold">{title}</p>
+          <p className="text-sm">{children}</p>
+        </div>
+        <div className="text-sm text-gray-400">
+          <p>
+            {date} &middot; {replyCount}개의 댓글
+          </p>
+        </div>
+      </div>
+      <div className="px-4 py-4 border-t-2 text-sm flex justify-between">
+        <div className="flex items-center">
+          {profile || <span className="w-6 h-6 bg-slate-400 rounded-full" />}&nbsp;
+          <span className="text-gray-400">by</span>&nbsp;
+          {author}
+        </div>
+        <span className="flex items-center">
+          <IoHeart />
+          &nbsp;{likeCount}
+        </span>
+      </div>
+    </div>
+  );
+}
+
+export default PreviewCard;
