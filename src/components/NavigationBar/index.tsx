7b847678c05c231397348e7ec782c6ddd1e7204d
Daegyeom Ha <combbm@gmail.com>
2022-12-14 20:32:10 +0900

    feat: add backgroundClass prop in NavigationBar

--- src/components/NavigationBar/index.tsx
+++ src/components/NavigationBar/index.tsx
@@ -2,9 +2,13 @@
 
 import React, { PropsWithChildren } from 'react';
 
-function NavigationBar({ children }: PropsWithChildren) {
+interface NavigationBarProps {
+  readonly backgroundClass?: string;
+}
+
+function NavigationBar({ children, backgroundClass = 'bg-slate-50' }: PropsWithChildren<NavigationBarProps>) {
   return (
-    <div className="w-full h-14 bg-slate-50 flex items-center">
+    <div className={`w-full h-14 flex items-center ${backgroundClass}`}>
       <div className="container mx-auto flex items-center justify-between px-10">{children}</div>
     </div>
   );
