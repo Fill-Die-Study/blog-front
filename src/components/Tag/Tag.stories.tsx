a61aaa1d2bb469426413e3bb37b641a58c9417a7
Daegyeom Ha <combbm@gmail.com>
2022-12-14 21:33:16 +0900

    feat: implement Tag component

---
+++ src/components/Tag/Tag.stories.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { ComponentMeta } from '@storybook/react';
+import TagComponent from './index';
+
+export default {
+  title: 'Tag',
+  component: TagComponent,
+  args: {
+    content: '내용'
+  },
+} as ComponentMeta<typeof TagComponent>;
+
+export function Tag({ ...args }) {
+  return <TagComponent {...args}>{args.content}</TagComponent>;
+}
---
+++ src/components/Tag/index.tsx
@@ -0,0 +1,13 @@
+'use client';
+
+import React, { PropsWithChildren } from 'react';
+
+function Tag({ children }: PropsWithChildren) {
+  return (
+    <div className="w-fit bg-gray-100 text-green-500 rounded-full px-3 py-1 text-md hover:bg-gray-50 hover:text-green-400 cursor-pointer">
+      {children}
+    </div>
+  );
+}
+
+export default Tag;
